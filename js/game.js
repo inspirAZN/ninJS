aa6b3053fed7d037b1d9ab0fb9b966190dd94262
Brian Chung <brianchung23@gmail.com>
2013-01-30 02:43:23 -0800

    Added Comments
    Comments yo

--- js/game.js
+++ js/game.js
@@ -1,10 +1,16 @@
+/**
+ * Authors: Joseph Caluza, Brian Chung
+ */
 window.onload = function() {
+	// init 600 x 700 area, black
 	Crafty.init(600,700);
 	Crafty.background("black");
-
+	
+	// load dat sprite
 	Crafty.sprite( 28 ,"/Users/josephcaluza/Desktop/newgame/images/FemaleNinjaSprite.png", {
 		ninja: [0,0,0,1.75],
 	});
-
+	
+	// our hero
 	var ninja = Crafty.e("2D, DOM, ninja, controls, Multiway").multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});	
 };

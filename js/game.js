467f1c2cf14e6d67d0f419a98f79ad4f840e4076
Joseph <bboyjc@gmail.com>
2013-01-31 06:29:26 -0800

    added animations
    some trial animations to see how the sprite sheets and .animate works

--- js/game.js
+++ js/game.js
@@ -6,8 +6,9 @@ window.onload = function() {
 	var bg = Crafty.init(1400,700);
 	
 	// load the sprites
-	Crafty.sprite( 28 ,"images/FemaleNinjaSprite.png", {
-		femaleNinja: [0,0,0,1.75],
+	Crafty.sprite( 32 ,"images/FemaleNinjaSprite.png", {
+		femaleNinja: [0,0,0,1.5],
+		femaleFrontWalk: [1,0,1,1.5]
 	});
 
 	Crafty.sprite( 32 ,"images/MaleNinjaSprite.png", {
@@ -32,9 +33,60 @@ window.onload = function() {
     }
   }
 
-	// our hero
-	var femaleNinja = Crafty.e("2D, DOM, femaleNinja, controls, Multiway").multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90});
 
+  	//trial animation (forward)
+
+  	Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
+    	.animate('PlayerRunning', 0, 0, 1.5) //setup animation
+    	.animate('PlayerRunning', 0, 0, 1.75)
+    	.animate('PlayerRunning', 0, 0, 2)
+    	.animate('PlayerRunning', 0, 0, 3)
+    	.animate('PlayerRunning', 40, -1) // start animation
+    	.attr({x: 300, y:300});
+
+    //trial animation (backward)
+
+    Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
+    	.animate('PlayerRunning2', 0, 4.5, 1) //setup animation
+    	.animate('PlayerRunning2', 0, 4.5, 2)
+    	.animate('PlayerRunning2', 0, 4.5, 2.5)
+    	.animate('PlayerRunning2', 0, 4.5, 3)
+    	.animate('PlayerRunning2', 40, -1) // start animation
+    	.attr({x: 340, y:300});
+
+    //trial animation (left)
+
+    Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
+    	.animate('PlayerRunning3', 0, 1.5, 1) //setup animation
+    	.animate('PlayerRunning3', 0, 1.5, 2)
+    	.animate('PlayerRunning3', 0, 1.5, 2.5)
+    	.animate('PlayerRunning3', 0, 1.5, 3)
+    	.animate('PlayerRunning3', 40, -1) // start animation
+    	.attr({x: 380, y:300});
+
+    //trial animation (right) something is wrong with this row of sprites
+
+    Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
+    	.animate('PlayerRunning4', 0, 3, 1) //setup animation
+    	.animate('PlayerRunning4', 0, 3, 2)
+    	.animate('PlayerRunning4', 0, 3, 2.5)
+    	.animate('PlayerRunning4', 0, 3, 3)
+    	.animate('PlayerRunning4', 40, -1) // start animation
+    	.attr({x: 420, y:300});
+
+
+	// female ninja
+	var femaleNinja = Crafty.e("2D, DOM, femaleNinja, controls, Multiway").multiway(4, {W: -90, S: 90, A: -90, D: 90})
+		.attr({x: 200, y: 350});
+
+	// male ninja
 	var maleNinja = Crafty.e("2D, DOM, maleNinja, controls, Multiway")
-		.multiway(3, {W: -90, S: 90});
+		.multiway(4, {UP_ARROW: -90, DOWN_ARROW: 90, LEFT_ARROW: -90, RIGHT_ARROW: 90})
+		.attr({x: 1050, y: 350});
+
+
+	 //seeing sprite positions	
+	 var femaleFrontWalk = Crafty.e("2D, DOM, femaleFrontWalk")
+		.attr({x: 500, y: 500});
+
 };

9505cab7a228d5624789c18d5978467a84110368
Joseph <bboyjc@gmail.com>
2013-01-31 00:27:23 -0800

    Fixed the up and down controls
    sprite now goes up and down instead of lefts and right

--- js/game.js
+++ js/game.js
@@ -16,20 +16,6 @@ window.onload = function() {
 		grass: [1,1],
 	});
 
-	//method to generate the map
-	function generateWorld() {
-    	//loop through all tiles
-    	for (var i = 0; i < 25; i++) {
-        	for (var j = 0; j < 21; j++) {
-
-            	//place grass on all tiles
-            	grassType = Crafty.math.randomInt(1, 4);
-            	Crafty.e("2D, DOM, grass," + grassType)
-                	.attr({ x: i * 16, y: j * 16, z:1 });
-		}
-	}
-};
 	// our hero
-	var ninja = Crafty.e("2D, DOM, ninja, controls, Multiway").multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180});
-
+	var ninja = Crafty.e("2D, DOM, ninja, controls, Multiway").multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90});
 };

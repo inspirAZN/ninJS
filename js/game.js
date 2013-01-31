1fb827e0eba130c7c99b087879f842d7ab827925
Joseph <bboyjc@gmail.com>
2013-01-31 04:10:35 -0800

    added ninja dude
    working on moving him to the other side of the screen

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ window.onload = function() {
 	// init 600 x 700 area, black
 	var bg = Crafty.init(600,700);
 	
+<<<<<<< HEAD
 	// load dat sprite
 	Crafty.sprite( 28 ,"images/FemaleNinjaSprite.png", {
 		ninja: [0,0,0,1.75],
@@ -13,6 +14,20 @@ window.onload = function() {
 	// some grass
 	Crafty.sprite(70, "images/grass.png", {
 		grass: [6,6]
+=======
+	// load the sprites
+	Crafty.sprite( 28 ,"/Users/josephcaluza/Desktop/newgame/images/FemaleNinjaSprite.png", {
+		femaleNinja: [0,0,0,1.75],
+	});
+
+	Crafty.sprite( 32 ,"/Users/josephcaluza/Desktop/newgame/images/MaleNinjaSprite.png", {
+		maleNinja: [0,0,0,1.5],
+	});
+	
+	// some grass
+	Crafty.sprite(639, "/Users/josephcaluza/Desktop/newgame/images/grass.png", {
+		grass: [0,0]
+>>>>>>> added ninja dude
 	});
 
   // Add grass
@@ -30,5 +45,8 @@ window.onload = function() {
 	//var grass = Crafty.e("2D, DOM, grass, sprite")
 
 	// our hero
-	var ninja = Crafty.e("2D, DOM, ninja, controls, Multiway").multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90});
+	var femaleNinja = Crafty.e("2D, DOM, femaleNinja, controls, Multiway").multiway(3, {UP_ARROW: -90, DOWN_ARROW: 90});
+
+	var maleNinja = Crafty.e("2D, DOM, maleNinja, controls, Multiway, attribute")
+		.multiway(3, {W: -90, S: 90});
 };

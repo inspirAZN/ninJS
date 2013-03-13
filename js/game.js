344920acaa4fafe2a9255b33e202bf0c466e8ae2
Joseph <bboyjc@gmail.com>
2013-03-12 19:56:48 -0700

    Deleted trials, multiple shuriken working
    creates too many shuriken

--- js/game.js
+++ js/game.js
@@ -5,9 +5,9 @@ window.onload = function() {
 
   var win_w = 1400;
   var win_h = 700;
-    // init 1400 x 700 area, black
-    var bg = Crafty.init(win_w, win_h);
-
+	// init 1400 x 700 area, black
+	var bg = Crafty.init(win_w, win_h);
+	
   /* 
    * Component definitions 
    */
@@ -86,7 +86,7 @@ window.onload = function() {
         return this;
     }
 });
-     
+     //setting up controls for right player
     Crafty.c("RightControls", {
         init: function() {
             this.requires('Multiway');
@@ -99,6 +99,7 @@ window.onload = function() {
          
     });
      
+     //setting up controls for left player
     Crafty.c("LeftControls", {
         init: function() {
             this.requires('Multiway');
@@ -111,32 +112,33 @@ window.onload = function() {
          
     });
 
-    // load the sprites
-    Crafty.sprite( 32 ,"images/FemaleNinjaSprite.png", {
-        femaleNinja: [0,0,0,1.5],
-        femaleFrontWalk: [1,0,1,1.5]
-    });
+	// load the sprites
+	Crafty.sprite( 32 ,"images/FemaleNinjaSprite.png", {
+		femaleNinja: [0,0,0,1.5],
+		femaleFrontWalk: [1,0,1,1.5]
+	});
 
-    Crafty.sprite( 32 ,"images/MaleNinjaSprite.png", {
-        maleNinja: [0,0,0,1.5],
-    });
+	Crafty.sprite( 32 ,"images/MaleNinjaSprite.png", {
+		maleNinja: [0,0,0,1.5],
+	});
 
     Crafty.sprite( 16 ,"images/shuriken2.png", {
         shuriken: [0,0,0,1],
     });
 
-
+	
     // some grass
   var grass_w = 639;
-    Crafty.sprite( grass_w/4 , "images/grass.png", {
-    grass1: [0,0],
+	Crafty.sprite( grass_w/4 , "images/grass.png", {
+	grass1: [0,0],
     grass2: [0,1],
     grass3: [0,2],
     grass4: [1,0]
-    });
+	});
 
     //shuriken things
     var shuriken = Crafty.e("2D, DOM, shuriken");
+
 
     //firing the shuriken
     Crafty.c('NinjaStar', {
@@ -155,14 +157,23 @@ window.onload = function() {
             return this;
         }
     });
-
+    var shoot = false;
     Crafty.c("FireControls", {
         init: function() {
             this.requires('Multiway');
         },
          
         fireControls: function(speed) {
-            this.multiway(speed, {SPACE: 0})
+            //this.multiway(speed, {SPACE: 0})
+            this.bind("KeyDown", function(e) {
+              if( e.keydown === Crafty.keys['SPACE'] ) {
+                shoot = true;
+                alert("ITS TRUE");
+              }
+            }).bind("EnterFrame", function() {
+              if( shoot )
+                this.x += speed;
+            });
             return this;
         }
          
@@ -178,8 +189,32 @@ window.onload = function() {
         return this;
     }
 });
-     
-    //trial animation (forward)
+
+    Crafty.c('ShurikenShoot', {
+      ShurikenShoot: function() {
+            Crafty.e("2D, DOM, NinjaStar, shuriken, Collision")
+            .attr({x: maleNinja.x + 30, y: maleNinja.y})
+        
+            .NinjaStar()
+            .bind("KeyDown", function(e) {
+              if( e.key === Crafty.keys["SPACE"] ) {
+                shoot = true;
+                //alert("ITS TRUE");
+              }
+        }).bind("EnterFrame", function() {
+              if( shoot )
+                this.x += 10;
+        }).onHit("femaleNinja", function() {
+          this.destroy();
+        });
+      }
+    });
+
+    // creating unlimited shuriken
+
+
+
+  	//trial animation (forward)
 
   //Crafty.e("2D, DOM, grass2, sprite");
 
@@ -192,125 +227,89 @@ window.onload = function() {
     }
   }
 
-    /*
-     * Trial Animation BEGIN ******
-     */
-
-    //trial animation (forward)
-    Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
-        .animate('PlayerRunning', 0, 0, 1.5) //setup animation
-        .animate('PlayerRunning', 0, 0, 1.75)
-        .animate('PlayerRunning', 0, 0, 2)
-        .animate('PlayerRunning', 0, 0, 3)
-        .animate('PlayerRunning', 40, -1) // start animation
-        .attr({x: 300, y:300});
-
-    //trial animation (backward)
-
-    Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
-        .animate('PlayerRunning2', 0, 4.5, 1) //setup animation
-        .animate('PlayerRunning2', 0, 4.5, 2)
-        .animate('PlayerRunning2', 0, 4.5, 2.5)
-        .animate('PlayerRunning2', 0, 4.5, 3)
-        .animate('PlayerRunning2', 40, -1) // start animation
-        .attr({x: 340, y:300});
-
-    //trial animation (left)
-
-    Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
-        .animate('PlayerRunning3', 0, 1.5, 1) //setup animation
-        .animate('PlayerRunning3', 0, 1.5, 2)
-        .animate('PlayerRunning3', 0, 1.5, 2.5)
-        .animate('PlayerRunning3', 0, 1.5, 3)
-        .animate('PlayerRunning3', 40, -1) // start animation
-        .attr({x: 380, y:300});
-
-    //trial animation (right) something is wrong with this row of sprites
-
-    Crafty.e("2D, DOM, SpriteAnimation, femaleNinja")
-        .animate('PlayerRunning4', 0, 3, 1) //setup animation
-        .animate('PlayerRunning4', 0, 3, 2)
-        .animate('PlayerRunning4', 0, 3, 2.5)
-        .animate('PlayerRunning4', 0, 3, 3)
-        .animate('PlayerRunning4', 40, -1) // start animation
-        .attr({x: 420, y:300});
-
-    //male ninja trial animations
-
-    //trial animation (forward)
-
-    Crafty.e("2D, DOM, SpriteAnimation, maleNinja")
-        .animate('PlayerRunning', 0, 0, 1.5) //setup animation
-        .animate('PlayerRunning', 0, 0, 1.75)
-        .animate('PlayerRunning', 0, 0, 2)
-        .animate('PlayerRunning', 0, 0, 3)
-        .animate('PlayerRunning', 40, -1) // start animation
-        .attr({x: 460, y:300});
-
-    //trial animation (backward)
-
-    Crafty.e("2D, DOM, SpriteAnimation, maleNinja")
-        .animate('PlayerRunning2', 0, 4.5, 1) //setup animation
-        .animate('PlayerRunning2', 0, 4.5, 2)
-        .animate('PlayerRunning2', 0, 4.5, 2.5)
-        .animate('PlayerRunning2', 0, 4.5, 3)
-        .animate('PlayerRunning2', 40, -1) // start animation
-        .attr({x: 500, y:300});
-
-    //trial animation (left)
-
-    //shuriken trial animations
-    Crafty.e("2D, DOM, SpriteAnimation, shuriken")
-        .animate('shurikenFire', 0, 0, 1) //setup animation
-        .animate('shurikenFire', 0, 1, 2)
-        .animate('shurikenFire', 0, 2, 3)
-        .animate('shurikenFire', 40, -1) // start animation
-        .attr({x: 600, y:300});
-
-    //trial animation (fire to the left)
-     Crafty.e("2D, DOM, SpriteAnimation, shuriken")
-        .animate('shurikenFire2', 0, 2, 2)//setup animation
-        .animate('shurikenFire2', 0, 0, 1)
-        .animate('shurikenFire2', 5, -1) // start animation
-        .attr({x: 640, y:300});
-    
-    //trial animation (fire to the right)
-
-    Crafty.e("2D, DOM, SpriteAnimation, maleNinja")
-        .animate('PlayerRunning', 0, 0, 1.5) //setup animation
-        .animate('PlayerRunning', 0, 0, 1.75)
-        .animate('PlayerRunning', 0, 0, 2)
-        .animate('PlayerRunning', 0, 0, 3)
-        .animate('PlayerRunning', 40, -1) // start animation
-        .attr({x: 460, y:300});
-
-    /*
-     * Trial Animation END ******
-     */
+   
 
   /*
    * Main Entity Initialization
    */
 
-    // female ninja
-    var femaleNinja = 
+   var __shuriSpd = 0;
+   var male_ninja_score = 0;
+   var female_ninja_score = 0;
+	// female ninja
+	var femaleNinja = 
   Crafty.e("2D, DOM, NinjaGirl, player, femaleNinja, RightControls, Collision")
         .attr({ x: 1000, y: 250, z: 0 })
                 .rightControls(2)
                 .NinjaGirl();
 
-    // male ninja
-    var maleNinja = 
-  Crafty.e("2D, DOM, NinjaBoy, player, maleNinja, LeftControls, Collision")
-        .attr({ x: 200, y: 250, z: 0 })
+	// male ninja
+	var maleNinja = 
+  Crafty.e("2D, DOM, NinjaBoy, player, maleNinja, LeftControls, Collision, ShurikenShoot")
+		.attr({ x: 200, y: 250, z: 0 })
         .leftControls(2)
-        .NinjaBoy();
+        .NinjaBoy()
+        .bind("KeyDown", function(e) {
+          if( e.key === Crafty.keys["SPACE"] ) {
+             //this.ShurikenShoot();   
+                //shuriken
+                var bulletX = this.x + 10;
+                var bulletY = this.y;
+
+                Crafty.e("2D, DOM, ShurikenShoot, Collision")
+                    .attr({
+                        x:bulletX,
+                        y:bulletY,
+                        speed:10
+                    }
+
+                        )
+            Crafty.e("2D, DOM, NinjaStar, shuriken, Collision")
+            .attr({x: maleNinja.x + 30, y: maleNinja.y})
+            //.fireControls(5)
+            .NinjaStar()
+            .bind("KeyDown", function(e) {
+                  if( e.key === Crafty.keys['SPACE'] ) {
+                    shoot = true;
+                    //alert("ITS TRUE");
+                  }
+            }).bind("EnterFrame", function() {
+                  if( shoot )
+                    this.x += 10;
+            }).onHit("femaleNinja", function() {
+              this.destroy();
+            });
+            Crafty.e("2D, Collision, DOM, shuriken, NinjaStar, FireControls")
+            .attr( {x: maleNinja.x, y: maleNinja.y, __shuriSpd: 10 } ) 
+            .NinjaStar()
+            .fireControls(15)
+            .bind("EnterFrame", function() {
+              this.x += __shuriSpd;
+            })
+            .onHit("femaleNinja", function() {
+              male_ninja_score++; this.destroy(); 
+            });
+          }
+    });
+
     
     //shuriken
-    var shuriken = Crafty.e("2D, DOM, shuriken, NinjaStar, FireControls")
-        .attr({x: maleNinja.x, y: maleNinja.y})
-        .fireControls(15)
-        .NinjaStar;
+    Crafty.e("2D, DOM, NinjaStar, shuriken, Collision")
+        .attr({x: maleNinja.x + 30, y: maleNinja.y})
+        //.fireControls(5)
+        .NinjaStar()
+        .bind("KeyDown", function(e) {
+              if( e.key === Crafty.keys['SPACE'] ) {
+                shoot = true;
+                //alert("ITS TRUE");
+              }
+        }).bind("EnterFrame", function() {
+              if( shoot )
+                this.x += 10;
+        }).onHit("femaleNinja", function() {
+          this.destroy();
+        });
+
 
   var borderTop =
   Crafty.e("2D, DOM, Collision, borderTop")
